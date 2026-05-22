1d204756cec672f9efc7b447364a252cb1a3e555
euroschneider-spec <euroschneider@gmail.com>
2026-05-22 10:58:46 +0200

    Update robots.txt for user-agent rules and sitemap

---
+++ public/robots.ts
@@ -0,0 +1,16 @@
+User-agent: *
+Allow: /
+Disallow: /api/
+Disallow: /admin/
+Disallow: /back-office/
+
+User-agent: GPTBot
+Allow: /
+
+User-agent: ClaudeBot
+Allow: /
+
+User-agent: PerplexityBot
+Allow: /
+
+Sitemap: https://www.lignorae.com/sitemap.xml
