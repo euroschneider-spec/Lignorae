375b77c8a82bf200b441bb3a4279433f72f66fc9
euroschneider-spec <euroschneider@gmail.com>
2026-05-22 10:47:23 +0200

    Add robots.txt generation for metadata route

---
+++ robot.ts
@@ -0,0 +1,26 @@
+import type { MetadataRoute } from "next";
+
+export default function robots(): MetadataRoute.Robots {
+  return {
+    rules: [
+      {
+        userAgent: "*",
+        allow: "/",
+        disallow: ["/api/", "/admin/", "/back-office/"],
+      },
+      {
+        userAgent: "GPTBot",
+        allow: "/",
+      },
+      {
+        userAgent: "ClaudeBot",
+        allow: "/",
+      },
+      {
+        userAgent: "PerplexityBot",
+        allow: "/",
+      },
+    ],
+    sitemap: "https://www.lignorae.com/sitemap.xml",
+  };
+}
